ec913642e407f19c32ec00d361a344965599d8a5
dohyeonj <jdh7693@naver.com>
2020-11-20 16:22:00 +0900

    Email verification add

---
+++ src/main/java/com/hiclub/email/ConsoleEmailService.java
@@ -0,0 +1,18 @@
+package com.hiclub.email;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Profile({"local", "dev"})
+@Component
+public class ConsoleEmailService implements EmailService{
+
+
+    @Override
+    public void sendEmail(EmailMessage emailMessage) {
+
+        log.info("sent email: {}", emailMessage.getMessage());
+    }
+}
---
+++ src/main/java/com/hiclub/email/EmailService.java
@@ -0,0 +1,6 @@
+package com.hiclub.email;
+
+public interface EmailService {
+
+    void sendEmail(EmailMessage emailMessage);
+}
