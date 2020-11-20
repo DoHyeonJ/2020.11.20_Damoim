c5e9dda0bb0a0e6944fdf3d3d826750a330da423
dohyeonj <jdh7693@naver.com>
2020-11-20 10:25:53 +0900

    create project

---
+++ src/main/java/com/hiclub/HiClubApplication.java
@@ -0,0 +1,13 @@
+package com.hiclub.hi_club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiClubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hiclub/HiClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiclub.hi_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
