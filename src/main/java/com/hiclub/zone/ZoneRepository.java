fb3fb48b692d16b78a426188fb8700a460d42d4d
dohyeonj <jdh7693@naver.com>
2020-11-26 11:37:57 +0900

    add zone domain and init data

---
+++ src/main/java/com/hiclub/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.hiclub.zone;
+
+import com.hiclub.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
